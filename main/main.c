d55a8a3b2ae640b447ffe8f868ee35f1b48f785a
Hbitz <53080862+Hbitz@users.noreply.github.com>
2026-07-27 11:14:27 +0200

    E-O6 fix - Uart and Sensor now has proper name arguments

--- main/main.c
+++ main/main.c
@@ -184,9 +184,9 @@ void app_main()
 
     xTaskCreate(ui_update_task, app.system_task_handlers.ui_task.name, app.system_task_handlers.ui_task.stack_size, &app, 5, &ui_task_handle);
 
-    xTaskCreate(UART_Work, &app.system_task_handlers.uart_task.name, app.system_task_handlers.uart_task.stack_size, &app, 4, &uart_task_handle);
+    xTaskCreate(UART_Work, app.system_task_handlers.uart_task.name, app.system_task_handlers.uart_task.stack_size, &app, 4, &uart_task_handle);
 
-    xTaskCreate(Sensor_Work, &app.system_task_handlers.sensor_task.name, app.system_task_handlers.sensor_task.stack_size, &app, 4, &sensor_task_handle);
+    xTaskCreate(Sensor_Work, app.system_task_handlers.sensor_task.name, app.system_task_handlers.sensor_task.stack_size, &app, 4, &sensor_task_handle);
 
 
     // Använd appens leop_data istället för en statisk lokal här.
